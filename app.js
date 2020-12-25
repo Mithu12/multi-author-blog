4f1f68b1ce3f2fee007736f53728595547fcc782
Mithu12 <mosaddekalimithu123@gmail.com>
2020-12-26 01:55:52 +0600

    view engine,initial middlewares added

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 // const Router = require('./routes/Routes')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
+// ========================================middle wares
+const middleWares = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended: true}),
+    express.json()
+]
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome</h1>')
 })
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(middleWares)
 
-// app.use(Router)     //without prefix
-
-// app.use('/f', Router)   //with prefix
-
+// ===================================setup view engine
 app.set('view engine', 'ejs')
+app.set('views', 'views')
 
 
 mongoose.connect('mongodb://localhost:27017/multi_user_blog', {useNewUrlParser: true, useUnifiedTopology: true})
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,7 +8,15 @@ const {
     logout
 } = require('../controller/authController')
 
+router.get('/signup', signup)
+router.post('/signup', postSignup)
 
+
+router.get('/login', login)
+router.post('/login', postLogin)
+
+
+router.get('logout', logout)
 
 
 module.exports = router
