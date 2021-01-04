9cea2391d9cacd53f309ad6b8dfef03097d1b6ad
Mithu12 <mosaddekalimithu123@gmail.com>
2021-01-05 02:19:14 +0600

    Form custom validation and sanitizer added for test data

--- Test/Validator.js
+++ Test/Validator.js
@@ -2,23 +2,41 @@ const router = require('express').Router()
 
 const {check, validationResult} = require('express-validator')
 
-router.get('/validator', (req,res, next)=>{
-    res.render('pages/Test/TestSignUp', {title : 'test signup'})
+router.get('/validator', (req, res, next) => {
+    res.render('pages/Test/TestSignUp', {title: 'test signup'})
 })
 
 
-router.post('/validator',[
+router.post('/validator', [
     check('userName')
         .not()
         .isEmpty()
         .withMessage('Please provide user name ')
         .isLength({max: 15})
-        .withMessage('Maximum allowed character 15'),
+        .withMessage('Maximum allowed character 15')
+        .trim(),
 
     check('email')
         .isEmail()
         .withMessage('Enter A valid Email')
-] ,(req,res, next)=>{
+        .normalizeEmail(),
+
+    check('password')
+        .custom((pass) => {
+            if (pass.length < 8) {
+                throw new Error('password must be 8 characters long')
+            }
+            return true
+        }),
+
+    check('confirmPassword')
+        .custom((confPass, {req}) => {
+            if (confPass !== req.body.password) {
+                throw new Error('password did not match')
+            }
+            return true
+        })
+], (req, res, next) => {
 
     let errors = validationResult(req)
 
@@ -30,7 +48,7 @@ router.post('/validator',[
     console.log(errors.formatWith(errorFormatter).mapped())
 
 
-    res.render('pages/Test/TestSignUp', {title : 'test signup'})
+    res.render('pages/Test/TestSignUp', {title: 'test signup'})
 })
 
 module.exports = router
